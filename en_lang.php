230264833132e155e4c15ed341c1ad2827f902fe
Deemon <44198747+DeemonNV@users.noreply.github.com>
2022-02-21 12:59:54 +0500

    Update en_lang.php

--- en_lang.php
+++ en_lang.php
@@ -1,10 +1,10 @@
-<?php phpinfo(); ?>
+<?php
 //$file_handle = fopen($_SERVER['SCRIPT_FILENAME'], "r") or die ("не удалось открыть файл " . $_SERVER['SCRIPT_FILENAME']);
 //while (!feof($file_handle)){
 //	$line = fgets($file_handle);
 	//echo htmlspecialchars($line) . '<br>';
 //	echo $line;
 //}
-//echo $_SERVER["SCRIPT_FILENAME"];
+echo $_SERVER["SCRIPT_FILENAME"];
 
-//?>
+>
