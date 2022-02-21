b750712c88c2b363b452d6db37a33f15b980dec3
Deemon <44198747+DeemonNV@users.noreply.github.com>
2022-02-21 12:55:14 +0500

    Update en_lang.php

--- en_lang.php
+++ en_lang.php
@@ -1,8 +1,9 @@
 <?php
-$file_handle = fopen($_SERVER['SCRIPT_FILENAME'], "r") or die ("не удалось открыть файл " . $_SERVER['SCRIPT_FILENAME']);
-while (!feof($file_handle)){
-	$line = fgets($file_handle);
+//$file_handle = fopen($_SERVER['SCRIPT_FILENAME'], "r") or die ("не удалось открыть файл " . $_SERVER['SCRIPT_FILENAME']);
+//while (!feof($file_handle)){
+//	$line = fgets($file_handle);
 	//echo htmlspecialchars($line) . '<br>';
-	echo $line;
-}
+//	echo $line;
+//}
+echo $_SERVER['SCRIPT_FILENAME'];
 ?>
